339a1eca5ec2286ad365912f754ad00cb6b8886a
IlyaBoykoProgr <44905605+IlyaBoykoProgr@users.noreply.github.com>
2019-11-16 14:02:52 +0300

    Add files via upload

--- widget-test/main.cpp
+++ widget-test/main.cpp
@@ -10,7 +10,8 @@ icon(WC_COMBOBOXW,L"Иконки",window,CBS_DROPDOWN),
 button(WC_COMBOBOXW,L"Кнопки",window,CBS_DROPDOWN),
 smessage(WC_BUTTONW,L"Готово");
 int paint(){
-    window->setTitle(L"Test widgets")->resize(400,400)->show();
+    if(isRestarted())window->message(L"Restarted.");
+    window->setTitle(L"Test widgets")->resize(400,400)->show()->minimize();
     SendMessageW(icon.hWnd,CB_ADDSTRING,0,(LPARAM)L"Ошибка");
     SendMessageW(icon.hWnd,CB_ADDSTRING,0,(LPARAM)L"Вопрос");
     SendMessageW(icon.hWnd,CB_ADDSTRING,0,(LPARAM)L"Восклицание");
@@ -24,8 +25,8 @@ int paint(){
     SendMessageW(button.hWnd,CB_ADDSTRING,0,(LPARAM)L"Повторить/Отмена");
     SendMessageW(button.hWnd,CB_ADDSTRING,0,(LPARAM)L"Отмена/Повторить/Продолжить");
 
-    tagRECT rect;GetWindowRect(window->hWnd,&rect);
-    resize(&rect);
+    tagRECT rect;
+    resize(&rect,0);
     return 0;
 }
 
@@ -34,18 +35,12 @@ void command(LPARAM &widgets){
         WCHAR txt[100], cptn[100];
         GetWindowTextW(text.hWnd,txt,100);
         GetWindowTextW(caption.hWnd,cptn,100);
-        short icon_id= SendMessageW(icon.hWnd,CB_GETCURSEL,0,0);
+        auto icon_id= SendMessageW(icon.hWnd,CB_GETCURSEL,0,0);
         if(icon_id==CB_ERR){
             window->message(L"Не выбран тип значка",L"Упс!",ERROR_ICO);
             return;
-        }
-        icon_id++; icon_id*=10;
-        switch(icon_id){
-        case 10:icon_id=ERROR_ICO;break;
-        case 20:icon_id=QUESTION;break;
-        case 30:icon_id=WARNING;break;
-        case 40:icon_id=INFORMATION;break;
-        }
+        }icon_id++; icon_id*=0x10;
+
         short button_id= SendMessageW(button.hWnd,CB_GETCURSEL,0,0);
         if(button_id==CB_ERR){
             window->message(L"Не выбраны кнопки ответа",L"Упс!",ERROR_ICO);
@@ -55,7 +50,7 @@ void command(LPARAM &widgets){
     }
 }
 
-void resize(RECT* newpos){
+void resize(RECT* newpos,int state){
     UNUSED(newpos);
     int q= window->height()-40;q/=6;
     int width=window->width();
@@ -66,4 +61,5 @@ void resize(RECT* newpos){
     icon.move(0,q*3)->resize(width-40,q+50);
     button.move(0,q*4)->resize(width-40,q+100);
     smessage.move(0,q*5)->resize(width,q);
+    if(state==SIZE_MINIMIZED)window->show();
 }

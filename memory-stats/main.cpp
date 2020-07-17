ab044bb726d10f42e096dd99764689865500b1bf
IlyaBoykoProgr <44905605+IlyaBoykoProgr@users.noreply.github.com>
2020-07-17 11:22:32 +0300

    This is new project!
    This program shows you RAM and CPU statistics in real-time!

---
+++ memory-stats/main.cpp
@@ -0,0 +1,59 @@
+#include "../APIfunc.h"
+#include "../FIGURES.h"
+using namespace std;
+
+const char* intPlusStr(int i,const char* str){
+    string buff=to_string(i);
+    return (buff+str).c_str();
+}
+const char* strPlusStr(const char* a, const char* b){
+    string buff=a;
+    return (buff+b).c_str();
+}
+
+int GetRam(){
+    MEMORYSTATUSEX statex;
+    statex.dwLength = sizeof (statex);
+    GlobalMemoryStatusEx (&statex);
+    return statex.dwMemoryLoad;
+}
+
+int GetCpu(){
+    static ULARGE_INTEGER TimeIdle, TimeKernel, TimeUser;
+    FILETIME Idle, Kernel, User;
+    ULARGE_INTEGER uIdle, uKernel, uUser;
+    GetSystemTimes(&Idle, &Kernel, &User);
+    memcpy(&uIdle, &Idle, sizeof(FILETIME));
+    memcpy(&uKernel, &Kernel, sizeof(FILETIME));
+    memcpy(&uUser, &User, sizeof(FILETIME));
+    long long t;
+    t = (((((uKernel.QuadPart-TimeKernel.QuadPart)+(uUser.QuadPart-TimeUser.QuadPart))-
+        (uIdle.QuadPart-TimeIdle.QuadPart))*(100))/((uKernel.QuadPart-
+        TimeKernel.QuadPart)+(uUser.QuadPart-TimeUser.QuadPart)));
+    TimeIdle.QuadPart = uIdle.QuadPart;
+    TimeUser.QuadPart = uUser.QuadPart;
+    TimeKernel.QuadPart = uKernel.QuadPart;
+    return static_cast<int>(t);
+}
+
+Window* pointer;
+
+int paint()
+{
+    window->destroy();
+    for(;;){
+        pointer=new Window((HWND)NULL);
+        int nRam=GetRam(),nCpu=GetCpu();
+        const char* ram=intPlusStr(nRam,"%RAM");
+        if(nRam<10)ram=strPlusStr("0",ram);
+        if(nRam==100)ram="FtoRAM";
+            printChar(pointer,10,30,ram,BLACK,nRam>80?RED:GREEN);
+        const char* cpu=intPlusStr(nCpu,"%CPU");
+        if(nCpu<10)cpu=strPlusStr("0",cpu);
+        if(nCpu==100)cpu="FtoCPU";
+            printChar(pointer,10,50,cpu,BLACK,nCpu>90?RED:GREEN);
+        Sleep(100);
+        delete pointer;
+    }
+    return 0;
+}
